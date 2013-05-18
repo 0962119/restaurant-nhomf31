3de5f0cc398ec3bbd3a4bae1dbbcdb0148a6ecf5
president.nguyenanhquoc@gmail.com <president.nguyenanhquoc@gmail.com@b0e59b9c-e15b-0ef6-8eeb-ba141d8d6b85>
2013-05-18 16:38:11 +0000

    Hoan Thành Việc test database, mọi người có thể code đồ án được rồi!

---
+++ trunk/Restaurant/DAO/DataProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.OleDb;
+using System.Data;
+using System.Xml;
+using System.IO;
+namespace DAO
+{
+    public class DataProvider
+    {
+        public static OleDbConnection ConnectDB()
+        {
+            //"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=QUANLYNHAHANG.accdb"
+            string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=QUANLYNHAHANG.accdb";
+            OleDbConnection conn;
+            conn = new OleDbConnection(connectionString);
+            conn.Open();
+            return conn;
+        }
+    }
+}
